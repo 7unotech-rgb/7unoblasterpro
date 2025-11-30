a648fbf1255e7e54e2bc617a733cd71c567cf241
7unotech-rgb <7unotech@gmail.com>
2025-11-30 21:11:16 +0700

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/vite-plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Memaksa output ke folder 'dist'
+  },
+  server: {
+    port: 3000,
+  }
 })
